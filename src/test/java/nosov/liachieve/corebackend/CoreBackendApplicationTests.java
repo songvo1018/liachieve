e929e9369e9b56a5d88f60b74582906900ce3e69
Aleksey Nosov <aleksey.nosov@amarcon.ru>
2023-09-16 13:46:07 +0600

    initial

---
+++ src/test/java/nosov/liachieve/corebackend/CoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nosov.liachieve.corebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
